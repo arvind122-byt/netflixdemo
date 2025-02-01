6bab30f320d12729fc6d644bd749329a5f623aec
arvind122-byt <bittuprabhas10@gmail.com>
2025-02-01 16:04:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 5
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "NANCY"
+vpc_security_group_ids = ["sg-0cec01e30cf3886ff"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
